72e94b2c0d6f70cbb6970e05da1b10c7d9d45d52
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-04-07 16:00:03 +0600

    complete

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -7,20 +7,27 @@ const Cart = ({ cart }) => {
 
   let totalPrice = 0;
   let totalShipping = 0;
+  let quantity = 0;
   for (const product of cart) {
-    totalPrice = totalPrice + product.price;
+    // if (product.quantity === 0) {
+    //   product.quantity = 1;
+    // }
+
+    // product.quantity = product.quantity || 1;
+    totalPrice = totalPrice + product.price * product.quantity;
     totalShipping = totalShipping + product.shipping;
+    quantity = quantity + product.quantity;
   }
   const tax = (totalPrice * 7) / 100;
   const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
-      <h4>Order Summary</h4>
-      <h2>Cart:{cart.length}</h2>
-      <p>Total Price: ${totalPrice}</p>
-      <p>Total Shipping: ${totalShipping}</p>
-      <p>Tax: ${tax.toFixed(2)}</p>
-      <h6>Grand Total: ${grandTotal.toFixed(2)}</h6>
+      <h2>Order Summary</h2>
+      <h4>Selected Items: {quantity}</h4>
+      <p>Total Price: $ {totalPrice}</p>
+      <p>Total Shipping: $ {totalShipping}</p>
+      <p>Tax: $ {tax.toFixed(2)}</p>
+      <h5>Grand Total: $ {grandTotal.toFixed(2)}</h5>
     </div>
   );
 };
