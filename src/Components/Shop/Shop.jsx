cc4b093ee68b0f94bbabc7282a9dd0d37a4493df
Rubaiyatrafi <rubaiyatkabirrafi9166@gmail.com>
2023-04-07 14:06:44 +0600

    database added

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Shop.css";
 import Product from "../Product/Product";
 import Cart from "../Cart/Cart";
+import { addToDb, getShoppingCart } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -12,10 +13,31 @@ const Shop = () => {
       .then((res) => res.json())
       .then((data) => setProducts(data));
   }, []);
+  useEffect(() => {
+    const storedCart = getShoppingCart();
+    const savedCart = [];
+    // step 1 get id
+    for (const id in storedCart) {
+      // step 2 get the product by uding id
+      const addedProduct = products.find((product) => product.id === id);
+
+      if (addedProduct) {
+        // step 3 get quantity of the product
+        const quantity = storedCart[id];
+        addedProduct.quantity = quantity;
+        // step 4 add the added product to the save cart
+        savedCart.push(addedProduct);
+      }
+      // console.log(addedProduct);
+    }
+    // step 5 set the cart
+    setCart(savedCart);
+  }, [products]);
 
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.id);
   };
 
   return (
